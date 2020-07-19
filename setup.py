c1ca49be89f76d3db8a6e6c807f286b690d1db7a
Leon Shams <52867365+schaall@users.noreply.github.com>
2020-07-19 16:13:26 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+  
+from setuptools import setup
+
+setup(
+  name="bezier_curve", 
+  version="0.1.0",
+  description="A simple program for creating bezier curves for complex decay.", 
+  url='https://github.com/mfiless/snake.git',
+  author='Leon Shams',
+  license='MIT',
+  zip_safe=False, 
+  python_requires='>=3.6'
+)
