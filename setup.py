00845ebc5287b5a53afd0da70573c3baf53d3b91
Leon Shams <52867365+schaall@users.noreply.github.com>
2020-07-19 16:19:29 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(
-  name="bezier_curve", 
+  name="bezier_decay", 
   version="0.1.0",
   description="A simple program for creating bezier curves for complex decay.", 
   url='https://github.com/mfiless/snake.git',
