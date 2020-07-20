e6cd56e203520fefeb8c2db736f1e4e34128e024
Leon Shams <52867365+schaall@users.noreply.github.com>
2020-07-19 18:28:24 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
   
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
   name="bezier_decay", 
   version="0.1.0",
+  author="Leon Shams",
   description="A simple program for creating bezier curves for complex decay.", 
   url='https://github.com/schaall/Bezier-Decay.git',
   author='Leon Shams',
   license='MIT',
-  packages=['bezier_decay'],
-  zip_safe=False, 
+  packages=find_packages(),
   python_requires='>=3.6'
+  entry_points={
+        "console_scripts": [
+            "bezier_decay = bezier_decay:bezier_decay",
+        ]
+    }
 )
